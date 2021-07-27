4dfa338d0e41b82d2bc1e76d2ed6c609ddc229cc
PavlosCl <Pablo.quijano@hotmail.cl>
2021-07-26 20:46:47 -0400

    User-CRUD

---
+++ WebServiceUsers-WCF/IWebServiceUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using WebServiceUsers_WCF.DTOs;
+
+namespace WebServiceUsers_WCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface IWebServiceUser
+    {
+        [OperationContract]
+        List<UserDTO> Get();
+
+        [OperationContract]
+        UserDTO GetUserById(int Id);
+
+        [OperationContract]
+        bool InsertUser(UserDTO User);
+
+        [OperationContract]
+        void UpdateUser(UserDTO User);
+
+        [OperationContract]
+        void DeleteUser(int Id);
+
+    }
+
+
+    
+}
---
+++ WebServiceUsers-WCF/WebServiceUser.svc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebServiceUsers_WCF.DAL.Entities;
+using WebServiceUsers_WCF.DTOs;
+
+namespace WebServiceUsers_WCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WebServiceUser : IWebServiceUser
+    {
+        private readonly PruebasContext DBContext = new PruebasContext();
+
+        public List<UserDTO> Get()
+        {
+                return DBContext.User.Select(
+            s => new UserDTO
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                UserName = s.UserName,
+                Password = s.Password,
+                EnrollmentDate = s.EnrollmentDate
+            }
+              ).ToList();
+        }
+
+        public UserDTO GetUserById(int Id)
+        {
+            return DBContext.User.Select(
+            s => new UserDTO
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                UserName = s.UserName,
+                Password = s.Password,
+                EnrollmentDate = s.EnrollmentDate
+            })
+        .FirstOrDefault(s => s.Id == Id);
+        }
+
+        public bool InsertUser(UserDTO User)
+        {
+            var entity = new User()
+            {
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                UserName = User.UserName,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+
+            DBContext.User.Add(entity);
+            DBContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public void UpdateUser(UserDTO User)
+        {
+            var entity = DBContext.User.FirstOrDefault(s => s.Id == User.Id);
+
+            entity.FirstName = User.FirstName;
+            entity.LastName = User.LastName;
+            entity.UserName = User.UserName;
+            entity.Password = User.Password;
+            entity.EnrollmentDate = User.EnrollmentDate;
+
+            DBContext.SaveChangesAsync();
+        }
+
+        public void DeleteUser(int Id)
+        {
+            var entity = new User()
+            {
+                Id = Id
+            };
+
+            DBContext.User.Attach(entity);
+            DBContext.User.Remove(entity);
+            DBContext.SaveChangesAsync();
+        }
+    }
+}
