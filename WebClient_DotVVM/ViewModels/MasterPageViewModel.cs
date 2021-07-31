cfa0ab87ab2bb6e91a31b4c48699d4bdd79ec0d3
PavlosCl <Pablo.quijano@hotmail.cl>
2021-07-31 19:00:08 -0400

    Add Web Client DotVVM

---
+++ WebClient_DotVVM/ViewModels/MasterPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace WebClient_DotVVM.ViewModels
+{
+    public class MasterPageViewModel : DotvvmViewModelBase
+    {
+    }
+}
